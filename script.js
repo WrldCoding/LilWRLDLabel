014fbfd32e6ffb190fd00afc995618c2a939e022
WrldCoding <lilwrldreal@gmail.com>
2020-12-16 05:17:59 +0000

    3.1 Beta -  Starting to introduce subscription

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ document.cookie = "APISID=VHnRSvySgbNloMwTGcrj9xx2jI0Lw35o5bT"
 
 document.cookie = "CGIC=ivJwW6Qunx63KG3QpEEhn0H5vB7RNwrUKm4w1jBKgXatz13ORIeHRnMIpMAkdoQ3d7PaHO95eHf04AmsNZgoofvzCYxjq9uT6kxENLvKMXY7Fw6vPKIqIZRZhk6MW";
 
-document.cookie = "google.com/=GCIDAdYnngWd4w6DC3b7bisUIRXHXvT1D0UuZf8ETpZlvprIdQEKzBK3HzaV095ponNnCJZatBrtA36Pqt9skki3qEzch19zWexfgYyR13nqL4P2MXFGGwlXK4aYrcqqK9"
+document.cookie = "google.com/=GCIDAdYnngWd4w6DC3b7bisUIRXHXvT1D0UuZf8ETpZlvprIdQEKzBK3HzaV095ponNnCJZatBrtA36Pqt9skki3qEzch19zWexfgYyR13nqL4P2MXFGGwlXK4aYrcqqK9";
 
+document.cookie = "AES KEY = gy2vb8gzcwb822sdh2sv7wmrr66swayv4np";
 
 var x = document.cookie;
 
